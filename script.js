73b1b8ea9f266cf219357e101b88a9315798a4c2
Kathe <katherinvanessa16@gmail.com>
2025-11-07 12:52:00 -0500

    Nave

---
+++ script.js
@@ -0,0 +1,144 @@
+var x = 100, y = 100;
+
+const KEY_ENTER = 13;
+const KEY_LEFT = 37;
+const KEY_UP = 38;
+const KEY_RIGHT = 39;
+const KEY_DOWN = 40;
+
+game = {
+    canvas: null,
+    ctx: null,
+    imagen: null,
+    caratula: null,
+    imagenEnemigo: null,
+    teclaPulada: null,
+    tecla: [],
+    colorBala: "red",
+    balas_array: new Array(),
+    enemigos_array: new Array(),
+    jugador: null,
+    x: 0
+};
+
+
+//---------------------- CLASES ----------------------
+
+function bala(x, y, w) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+
+    this.dibujar = function () { }
+}
+
+function jugador(x) {
+    this.x = x;
+    this.y = 450;
+
+    this.dibujar = function (x) {
+        this.x = x;
+        game.ctx.drawImage(game.imagen, this.x, this.y, 30, 15);
+    }
+}
+
+function enemigo(x, y) {
+    this.x = x;
+    this.y = y;
+    this.w = 35;
+    this.veces = 0;
+    this.dx = 5;
+    this.ciclos = 0;
+    this.num = 14;
+    this.fugura = true;
+    this.vive = true;
+    this.dibujar = function () { }
+}
+
+
+//---------------------- FUNCIONES ----------------------
+
+const caratula = () => {
+    let imagen = new Image();
+    imagen.src = "imagenes/cara.webp";
+
+    imagen.onload = function () {
+        game.caratula = true;   
+        game.ctx.drawImage(imagen, 0, 0, game.canvas.width, game.canvas.height);
+    }
+}
+
+const selecionar = () => {
+    if (game.caratula) {
+        inicio();
+    }
+}
+
+const inicio = () => {
+    game.ctx.clearRect(0, 0, game.canvas.width, game.canvas.height);
+    game.caratula = false;
+
+    game.jugador = new jugador(0);
+    game.x = game.canvas.width / 2;
+
+    game.jugador.dibujar(game.x);
+
+    animar();
+}
+
+
+const animar = () => {
+    requestAnimationFrame(animar);
+    verificar();
+    pintar();
+}
+
+
+const verificar = () => {
+    if (game.tecla[KEY_LEFT]) {
+        game.x -= 10;
+    }
+    if (game.tecla[KEY_RIGHT]) {
+        game.x += 10;
+    }
+}
+
+const pintar = () => {
+    game.ctx.clearRect(0, 0, game.canvas.width, game.canvas.height);
+    game.ctx.beginPath();
+    game.jugador.dibujar(game.x);
+}
+
+
+//---------------------- LISTENERS ----------------------
+
+window.addEventListener("keydown", function (e) {
+    game.teclaPulada = e.keyCode;
+    game.tecla[e.keyCode] = true;
+});
+
+window.addEventListener("keyup", function (e) {
+    game.tecla[e.keyCode] = false;
+});
+
+
+window.requestAnimationFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        function (callback) { window.setTimeout(callback, 17); }
+})();
+
+
+//---------------------- INICIO ----------------------
+
+window.onload = function () {
+    game.canvas = document.getElementById("canvas");
+    game.ctx = game.canvas.getContext("2d");
+
+    game.imagen = new Image();
+    game.imagen.src = "imagenes/torre.png";
+
+    caratula();
+
+    game.canvas.addEventListener("click", selecionar, false);
+}
